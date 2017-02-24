0f39130c7d0b9a46597516ce0865752bc302a56d
Mantas Mantas <Mantas Mantas>
2017-02-24 16:43:30 +0200

    Calculation for loan information

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {CalculatorService} from "./calculator.service";
+import {variable} from "@angular/compiler/src/output/output_ast";
 
 @Component({
   selector: 'app-calculator',
@@ -16,6 +17,76 @@ export class CalculatorComponent implements OnInit {
   ngOnInit() {
   }
 
-  
+  loanInformation(loanSum:number, loanPeriod:number):Array<Payment>{
+
+    var paymentList:Payment[] = new Array();
+
+    var leftSum:number = loanSum;
+    var monthlyInterestRate:number = 0.16 / 12 ;
+
+    var monthlySum:number = ((loanSum * monthlyInterestRate)/(1-Math.pow((1+monthlyInterestRate),(-1 * loanPeriod))))+ 0.7;
+
+    for(var currentMonth:number = 0; currentMonth < loanPeriod; currentMonth++){
+      paymentList.push(this.calculatePayment(currentMonth, leftSum, monthlySum, monthlyInterestRate));
+      leftSum = leftSum - (monthlySum - (leftSum * monthlyInterestRate));
+    }
+
+    return paymentList;
+
+}
+
+  calculatePayment(currentMonth:number, leftSum:number, monthlySum:number, monthlyInterestRate:number):Payment{
+
+    return new Payment(currentMonth+1, leftSum, monthlySum, leftSum*monthlyInterestRate);
+}
+
+}
+
+class Payment{
+
+    private _monthNumber:number;
+    private _leftSum:number;
+    private _monthlySum:number;
+    private _monthlyInterestSum:number;
+    usualServiceSum:number = 0.70;
+
+  constructor(monthNumber: number, leftSum: number, monthlySum: number, monthlyInterestSum: number) {
+    this._monthNumber = monthNumber;
+    this._leftSum = leftSum;
+    this._monthlySum = monthlySum;
+    this._monthlyInterestSum = monthlyInterestSum;
+  }
+
+  get monthNumber(): number {
+    return this._monthNumber;
+  }
+
+  set monthNumber(value: number) {
+    this._monthNumber = value;
+  }
+
+  get leftSum(): number {
+    return this._leftSum;
+  }
+
+  set leftSum(value: number) {
+    this._leftSum = value;
+  }
+
+  get monthlySum(): number {
+    return this._monthlySum;
+  }
+
+  set monthlySum(value: number) {
+    this._monthlySum = value;
+  }
+
+  get monthlyInterestSum(): number {
+    return this._monthlyInterestSum;
+  }
+
+  set monthlyInterestSum(value: number) {
+    this._monthlyInterestSum = value;
+  }
 
 }
