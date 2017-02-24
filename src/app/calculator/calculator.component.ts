5d1a4db9e6ef8b92d4ccbb6c300e1b975e4fb3d3
adtm <tomas.eglinskas@gmail.com>
2017-02-24 17:25:04 +0200

    [Dev] Added lower slider information

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,13 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  loanSum: number;
+  loanTime: number;
 
+  loanPersonMoney: number;
+  loanSpouseMoney: number;
 
-  constructor() { }
+  constructor() {
+    // sutvarkyti !!
+    setTimeout(() => this.loanSum = 245);
+    setTimeout(() => this.loanTime = 1);
+    setTimeout(() => this.loanPersonMoney = 500);
+    setTimeout(() => this.loanSpouseMoney = 500);
+  }
 
   ngOnInit() {
   }
-
-
 
 }
