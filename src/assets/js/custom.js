69263c1ca857e24399e3d7d88b208df247fee9d5
adtm <tomas.eglinskas@gmail.com>
2017-02-26 15:03:36 +0200

    [Dev] minimum and negative input fix

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -4,3 +4,22 @@ $('input[name="loan-type"]').on('change', function (event) {
     hasSpouse = radioButton.val() === 'spouse' && radioButton.prop('checked');
   $('.extraInfoSpouse').toggleClass('hidden', !hasSpouse);
 }).change();
+
+// pakeisiu
+$("body").delegate('.requiredLoan', 'focusout', function(){
+  if($(this).val() < 500){
+    $(this).val('245');
+  }
+});
+
+$("body").delegate('.minimumPay', 'focusout', function(){
+  if($(this).val() < 500){
+    $(this).val('500');
+  }
+});
+
+$("body").delegate('.minimumTime', 'focusout', function(){
+  if($(this).val() < 1){
+    $(this).val('1');
+  }
+});
