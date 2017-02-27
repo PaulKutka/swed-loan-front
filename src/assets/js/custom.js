033097822678cfe9c68d55c1ff8f183927bfc20c
adtm <tomas.eglinskas@gmail.com>
2017-02-27 14:13:58 +0200

    error comments

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -5,3 +5,14 @@ $('input[name="loan-type"]').on('change', function (event) {
   $('.extraInfoSpouse').toggleClass('hidden', !hasSpouse);
 }).change();
 
+// $('.documentCodeInput').on('change', function(){
+//   var input = $(this).val();
+//   console.log(input.length);
+//   if(input.length < 11 ) {
+//
+//     var allInputs = document.querySelectorAll('.documentCodeError');
+//     console.log(allInputs);
+//     allInputs.toggleClass('error');
+//     return false;
+//   }
+// });
