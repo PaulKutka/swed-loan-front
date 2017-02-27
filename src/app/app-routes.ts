c59ef8474d00b8f98bb6507ac38b87e94c280549
adtm <tomas.eglinskas@gmail.com>
2017-02-27 14:38:34 +0200

    merge with calculator

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,16 +1,20 @@
-
-import {RouterModule, Routes} from "@angular/router";
+import {Routes, RouterModule} from "@angular/router";
+import {FormComponent} from "./form/form.component";
 import {CalculatorComponent} from "./calculator/calculator.component";
 import {LoanTablesComponent} from "./loan-tables/loan-tables.component";
 
 const APP_ROUTES: Routes = [
   {
     path: '',
-    component: CalculatorComponent,
+    component: CalculatorComponent
   },
   {
     path: '',
     component: LoanTablesComponent
+  },
+  {
+    path: 'formCompletion',
+    component: FormComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
-import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
-
 import {AppComponent} from "./app.component";
 import {CalculatorComponent} from "./calculator/calculator.component";
+import {LoanTablesComponent} from "./loan-tables/loan-tables.component";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 import {APP_ROUTES_PROVIDER} from "./app-routes";
-import {LoanTablesComponent} from './loan-tables/loan-tables.component';
+import {FormComponent} from "./form/form.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
-    LoanTablesComponent
+    LoanTablesComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
