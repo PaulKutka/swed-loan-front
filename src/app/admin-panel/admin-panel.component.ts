dbb50f6740c4b3378a19d00d9fdb0f7a0b6491d8
adtm <tomas.eglinskas@gmail.com>
2017-03-01 23:52:44 +0200

    fixed modal close

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -1,5 +1,4 @@
-
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, Input, HostBinding} from "@angular/core";
 import {AdminsPanelService} from "./admins-panel.service";
 import {FormService} from "../form/form.service";
 
@@ -17,7 +16,10 @@ export class AdminPanelComponent implements OnInit {
   adminPanels: any;
   error: any;
 
-  constructor(private adminPanel: AdminsPanelService) {}
+  public isCollapsedContent:boolean = false;
+
+  constructor(private adminPanel: AdminsPanelService) {
+  }
 
   ngOnInit() {
     this.getLoanList();
@@ -36,9 +38,70 @@ export class AdminPanelComponent implements OnInit {
 
 
   changeStatus(status, id) {
- this.adminPanel.changeStatus(status, id);
+    this.adminPanel.changeStatus(status, id);
+    this.isCollapsedContent = !this.isCollapsedContent
   };
 
+  @HostBinding('style.height')
+  private height:string;
+  // shown
+  @HostBinding('class.in')
+  @HostBinding('attr.aria-expanded')
+  private isExpanded:boolean = true;
+  // hidden
+  @HostBinding('attr.aria-hidden')
+  private isCollapsed:boolean = false;
+  // stale state
+  @HostBinding('class.collapse')
+  private isCollapse:boolean = true;
+  // animation state
+  @HostBinding('class.collapsing')
+  private isCollapsing:boolean = false;
+
+  @Input()
+  private set collapse(value:boolean) {
+    this.isExpanded = value;
+    this.toggle();
+  }
+
+  private get collapse():boolean {
+    return this.isExpanded;
+  }
+
+  toggle() {
+    if (this.isExpanded) {
+      this.hide();
+    } else {
+      this.show();
+    }
+  }
+
+  hide() {
+    this.isCollapse = false;
+    this.isCollapsing = true;
+
+    this.isExpanded = false;
+    this.isCollapsed = true;
+    setTimeout(() => {
+      this.height = '0';
+      this.isCollapse = true;
+      this.isCollapsing = false;
+    }, 4);
+  }
+
+  show() {
+    this.isCollapse = false;
+    this.isCollapsing = true;
+
+    this.isExpanded = true;
+    this.isCollapsed = false;
+    setTimeout(() => {
+      this.height = 'auto';
+
+      this.isCollapse = true;
+      this.isCollapsing = false;
+    }, 4);
+  }
 
 
 }
